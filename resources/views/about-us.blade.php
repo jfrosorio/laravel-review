8d55c0416325dd981bab12395c5fffee15c7055c
José Osório <jfrosorio@gmail.com>
2022-10-04 17:46:00 +0100

    Revert "feat(pages): about us page development"
    This reverts commit dfc54c3667851d9a869b5171f09ba2e21f8b01d9.

--- resources/views/about-us.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('page_title', $pageTitle)
-
-@section('content')
-    <p>Custom content for the Contacts page</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'homepage'])->name('homepage');
-Route::get('about-us', [PageController::class, 'aboutUs'])->name('about-us');
 Route::get('contacts', [PageController::class, 'contacts'])->name('contacts');
 
 Route::resource('cars', \App\Http\Controllers\CarController::class);
