c73f72ddb1034f83587e548c02956d2b33fc0037
José Osório <jfrosorio@gmail.com>
2022-10-04 18:00:37 +0100

    feat(pages): products page development

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
             <ul>
                 <li><a href="{{ route('homepage') }}">Homepage</a></li>
                 <li><a href="{{ route('about-us') }}">About us</a></li>
+                <li><a href="{{ route('about-us') }}">Products</a></li>
                 <li><a href="{{ route('contacts') }}">Contacts</a></li>
             </ul>
         </nav>
